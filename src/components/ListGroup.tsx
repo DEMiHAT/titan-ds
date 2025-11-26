02f7e06605c4332c5e61208e05da7a24c6020d07
Sanjeev S <116657701+DEMiHAT@users.noreply.github.com>
2025-11-26 17:55:57 +0530

    Add files via upload

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,26 @@
+import {Fragment} from 'react';
+function ListGroup() {
+    const items = [
+        'New York',
+        'San Francisco',
+        'Tokyo',
+        'London',
+        'Paris'
+    ];
+}
+
+    return (
+            <>
+            <h1>List</h1>
+            {items.length === 0 ?<p>No item Found</p> : null}
+            <ul className="list-group">
+                {items.map(item => (
+                    <li key={item}>{item}</li>
+                ))}
+            </ul>
+            </>
+    );
+}
+
+
+export default ListGroup;
